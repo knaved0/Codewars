a3249053ec726ee02b7740c127303d29f9674f65
knaved0 <105084176+knaved0@users.noreply.github.com>
2022-05-25 14:46:49 -0400

    Create BooleanToString.java

---
+++ BooleanToString.java
@@ -0,0 +1,9 @@
+/*Implement a function which convert the given boolean value into its string representation. */
+
+public class BooleanToString {
+    public static String convert(boolean b) {
+        String output = Boolean.toString(b);
+        return output;
+
+    }
+}
