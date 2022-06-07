e5f2e946c402404f71c2ab6775967b7d64569b7c
knaved0 <105084176+knaved0@users.noreply.github.com>
2022-06-07 15:37:59 -0400

    Create isSquare.java

---
+++ isSquare.java
@@ -0,0 +1,12 @@
+//Given an integral number, determine if it's a square number
+
+public public class Square {    
+    public static boolean isSquare(int n) {          
+
+int sqrt = (int) Math.sqrt(n);  
+
+return (sqrt*sqrt == n) ? true : false;
+}
+      } isSquare {
+    
+}
