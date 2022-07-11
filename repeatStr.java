2f6377c2fc007e6831e757fc22bc12b32fb27e7e
knaved0 <105084176+knaved0@users.noreply.github.com>
2022-07-11 19:48:12 -0400

    Create repeatStr.java

---
+++ repeatStr.java
@@ -0,0 +1,7 @@
+/*Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times. */
+
+public class Solution {
+    public static String repeatStr(final int n, final String string) {
+      return ( string.repeat(n) );
+    }
+}
